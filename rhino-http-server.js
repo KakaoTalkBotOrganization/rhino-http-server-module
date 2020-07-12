0a8ae26b47d33351bad92eb44b95009123e63d9a
hui1601 <hui1601@naver.com>
2020-07-12 19:08:36 +0900

    update

--- rhino-http-server.js
+++ rhino-http-server.js
@@ -5,9 +5,11 @@ const InputStreamReader = java.io.InputStreamReader;
 const BufferedReader = java.io.BufferedReader;
 exports.HttpServer = function() {
     function HttpServer() {
+		this.server = {}
 		this.server.listener = null;
 		this.server.body = "";
 		this.server.status = "200 OK";
+		this.server.head = {};
 		this.server.head["Keep-Alive"] = "timeout=5, max=100";
 		this.server.head["Connection"] = "Keep-Alive";
 		this.server.head["Content-Type"] = "text/html; charset=utf8";
@@ -19,41 +21,50 @@ exports.HttpServer = function() {
 	HttpServer.prototype.isRunning = false;
 	HttpServer.prototype.start = function(listener) {
 		HttpServer.prototype.isRunning = true;
-		this.server.thread = java.lang.Thread(() -> {
-			while(HttpServer.prototype.isRunning) {
-				let sock = listener.accept();
-				let reader = BufferedReader(InputStreamReader(socket.getInputStream()));
-				let client = {};
-				let line = reader.readLine();
-				let s = line.split(" ");
-				client["method"] = s[0];
-				client["request_uri"] = s[1];
-				delete s;
-				client["HTTP Request"] = line;
-				while ((line = reader.readLine()) != null)
-				{
-					let s = line.split(": ");
-					let key = s[0];
-					s.shift();
-					let value = s.join(": ");
+		let runnable = new java.lang.Runnable( {
+			run: function(){
+				while(HttpServer.prototype.isRunning) {
+					let sock = this.server.listener.accept();
+					let reader = BufferedReader(InputStreamReader(sock.getInputStream()));
+					let client = {};
+					let line = reader.readLine();
+					if(line == null) throw "can not read input stream";
+					let s = line.split(" ");
+					client["method"] = s[0];
+					client["request_uri"] = s[1];
 					delete s;
-					client[key] = value;
+					client["HTTP Request"] = line;
+					while ((line = reader.readLine()) != "")
+					{
+						let s = line.split(": ");
+						let key = s[0];
+						s.shift();
+						let value = s.join(": ");
+						delete s;
+						client[key] = value;
+					}
+					listener(this, client);//client["Host"]이런식으로 접근
+					let response = "HTTP/1.1 " + this.server.status + "\r\n";
+					response += "Content-Length: " + this.server.body.length + "\r\n";
+					Object.keys(this.server.head).forEach(key => {
+						response += key + ": "+ this.server.head[key] + "\r\n";
+					});
+					response += "\r\n" + this.server.body
+					PrintWriter(sock.getOutputStream(), true).println(response);
+					sock.close();
 				}
-				listener(this, client);//client["Host"]이런식으로 접근
-				let response = "HTTP/1.1 " + this.server.status + "\r\n";
-				response += "Content-Length: " + this.server.body.length + "\r\n";
-				Object.keys(this.server.head).forEach(key => {
-					response += key + ": "+ this.server.head[key] + "\r\n";
-				});
-				response += "\r\n" + this.server.body
-				PrintWriter(sock.getOutputStream(), true).println(response);
-				sock.close();
 			}
 		});
+		this.server.thread = new java.lang.Thread(runnable);
 		this.server.thread.start();
 	};
 	HttpServer.prototype.stop = function() {
 		HttpServer.prototype.isRunning = false;
-		this.server.thread.join();
+		this.server.thread.interrupt();
+		if(this.server.listener != null)
+		{
+			this.server.listener.close();
+		}
 	};
+	return HttpServer;
 }
